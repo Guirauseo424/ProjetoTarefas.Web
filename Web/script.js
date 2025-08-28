a696cf46e81c3314310dc1aa9b66af98200bacce
Guilherme Rauseo Luz <grluz7@gmail.com>
2025-08-28 16:00:23 -0300

    Salvo

--- Web/script.js
+++ Web/script.js
@@ -1,8 +1,16 @@
+const tarefas = []
 const botao = document.querySelector("form")
 botao.addEventListener("submit", function(event){event.preventDefault()
 
 const tarefa = document.querySelector("input")
 const textotarefa = tarefa.value
+const novastarefas = {
+    texto : textotarefa,
+    concluida: false,
+}
+
+tarefas.push(novastarefas)
+console.log(tarefas)
 
 const novatarefa = document.createElement("li")
 novatarefa.textContent = textotarefa
