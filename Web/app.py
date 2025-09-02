ee6d94cec0081078a07aaa8a3de7c5133fae7d1c
Guilherme Rauseo Luz <grluz7@gmail.com>
2025-09-02 15:47:26 -0300

    Comentarios

--- Web/app.py
+++ Web/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template  
+# Importa ferramentas
+# Flask cria aplicações 
+# render_template serve os arquivos HTML
+# jsonfy tranforma dados de pyhton em json, língua que JS entende 
+from flask import Flask, render_template, jsonify  
+                                                  
+app = Flask(__name__) # Cria aplicação Web
 
-app = Flask(__name__)
+listaTarefa = [] # Lista que vai ser o banco de dados 
 
-@app.route("/")
-def hello_world():
+@app.route("/") #Rota para a página principal, quando a função é executada chama essa rota 
+def pagina_inicial(): # Função retorna o arquivo HTML puxando pela pasta "templates"
     return render_template("index.html")
 
+@app.route("/api/tarefas") # Rotas da API, retorna apenas os dados
+def obter_tarefa(): # Função que retorna pyhton em json para o JS ler
+    return jsonify(listaTarefa)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) # Se executar o arquivo liga o servidor
--- Web/static/script.js
+++ Web/static/script.js
@@ -1,28 +1,33 @@
-const tarefas = []
+
+const tarefas = [] // Lista que guarda todas as tarefas como objetos 
 const botao = document.querySelector("form")
-botao.addEventListener("submit", function(event){event.preventDefault()
+const tarefa = document.querySelector("input") // Cria uma variável para receber os valores em HTML
+const listatarefa = document.querySelector("ul") 
 
-const tarefa = document.querySelector("input")
-const textotarefa = tarefa.value
-const novastarefas = {
-    texto : textotarefa,
-    concluida: false,
-}
+botao.addEventListener("submit", function(event){event.preventDefault() // Função para clicar no botão
+    const textotarefa = tarefa.value // Pega o texto que o usuário digitou no momento do envio
+    const novastarefas = {
+        texto : textotarefa,
+        concluida: false,       // Cria um objeto para a tarefa que acabou de receber 
+    }
 
-tarefas.push(novastarefas)
-console.log(tarefas)
-renderizarTarefa()
+    tarefas.push(novastarefas) // Adiciona o texto do usuário para a lista de objetos 
+    console.log(tarefas) // Temporário
+    renderizarTarefa() // Chama outra função
 })
 
-function renderizarTarefa(){
+function renderizarTarefa(){ // Função para pegar a lista de tarefas e "desenhar" na tela
+    listatarefa.innerHTML = "" // Limpa a lista para não ter duplicados
 
-const listatarefa = document.querySelector("ul")
-listatarefa.innerHTML = ""
-
-for(const i of tarefas){
-    let novalinha = document.createElement("li")
-    novalinha.textContent = i.texto
-    listatarefa.appendChild(novalinha)
+    for(const i of tarefas){    // Criar um laço de repetção para rodar pela lista de tarefas
+        let novalinha = document.createElement("li") // Cria um elemento vazio "li" vazio na memória 
+        novalinha.textContent = i.texto // O novo elemento criado recebe o valor da vez da lista de tarefas
+        listatarefa.appendChild(novalinha) // Adiciona o novo elemento que agora tem valor dentro da "ul" do HTML
+    }
 }
+
+function buscar_tarefas(){
+    fetch("/api/tarefas")
+    .then 
 }
 
